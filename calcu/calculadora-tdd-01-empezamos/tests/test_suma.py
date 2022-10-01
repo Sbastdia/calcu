e5568badd0739450203d7f6d8a1ef5744fbb0d56
Sbastdia <sbastdia@myuax.com>
2022-10-01 18:43:43 +0200

    perfeccionando

--- calcu/calculadora-tdd-01-empezamos/tests/test_suma.py
+++ calcu/calculadora-tdd-01-empezamos/tests/test_suma.py
@@ -1,1 +1,3 @@
+import sys
+sys.path.insert(1, 'C:/SARA/UAX/3º CURSO/Primer cuatrimestre/Desarrollo Orientado a Objetos/calcu/calculadora-tdd-01-empezamos/src')
 from src.calculadora import Calculator
