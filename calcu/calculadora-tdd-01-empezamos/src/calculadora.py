a6bb23eeadc8369ce583586e958d6038cd875a2f
Sbastdia <sbastdia@myuax.com>
2022-10-01 18:38:15 +0200

    escribiendo código

--- calcu/calculadora-tdd-01-empezamos/src/calculadora.py
+++ calcu/calculadora-tdd-01-empezamos/src/calculadora.py
@@ -5,3 +5,23 @@ class Calculator:
 
     def add(self, a, b):
         self.value = a + b
+
+    def rest(self, a, b):
+        self.value = a - b
+
+    def prod(self, a, b):
+        self.value = a * b
+
+    def div(self, a, b):
+        self.value = a/b
+
+    def fact(self, a):
+        if a<0:
+            self.value="No se puede hacer el factorial de un número negativo"
+        else:
+            fact=1
+            for i in range(1, a+1):
+                fact= fact*i
+            self.value=fact
+
+
--- calcu/calculadora-tdd-01-empezamos/tests/test_calculator.py
+++ calcu/calculadora-tdd-01-empezamos/tests/test_calculator.py
@@ -16,3 +16,45 @@ class TestMyCalculator(unittest.TestCase):
         self.calc.add(1, 3)
         # Comprobamos si el valor es el que esperamos
         self.assertEqual(4, self.calc.value)
+
+    #Creamos un nuevo test para comprobar una resta
+    def test_rest_method(self):
+        # Ejecutamos el método
+        self.calc.rest(5, 3)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(2, self.calc.value)
+
+    #Creamos un nuevo test para comprobar un producto
+    def test_prod_method(self):
+        # Ejecutamos el método
+        self.calc.prod(5, 3)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(15, self.calc.value)
+
+    #Creamos un nuevo test para comprobar una división
+    def test_div_method(self):
+        # Ejecutamos el método
+        self.calc.div(4, 2)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(2, self.calc.value)
+
+    #Creamos un nuevo test para comprobar un factorial
+    def test_fact_method(self):
+        # Ejecutamos el método
+        self.calc.fact(3)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(6, self.calc.value)
+
+    #Creamos un nuevo test para comprobar un factorial de 0
+    def test_fact0_method(self):
+        # Ejecutamos el método
+        self.calc.fact(0)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(1, self.calc.value)
+
+    #Creamos un nuevo test para comprobar un factorial de un número negativo
+    def test_factNeg_method(self):
+        # Ejecutamos el método
+        self.calc.fact(-1)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual("No se puede hacer el factorial de un número negativo", self.calc.value)
